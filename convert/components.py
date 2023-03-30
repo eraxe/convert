fb00f60f2c2a1c5e90359d30e87816d20dcbe065
arash <parppl@gmail.com>
2023-03-30 12:51:40 +0330

    fixing txt to po and VV functions

--- convert/components.py
+++ convert/components.py
@@ -1,7 +1,31 @@
 import polib
 
-def po_to_txt(po_filepath: str, txt_filepath: str):
-    po = polib.pofile(po_filepath)
-    with open(txt_filepath, "w") as txt_file:
+def po_to_txt(po_file):
+    po = polib.pofile(po_file)
+    with open(f"{po_file}_output.txt", "w") as txt_file:
         for entry in po:
-            txt_file.write(f"{entry.msgid} -> {entry.msgstr}\n")
+            if entry.msgid_plural:
+                txt_file.write(f"{entry.msgid} -> {entry.msgstr[0]}\n")
+                txt_file.write(f"{entry.msgid_plural} -> {entry.msgstr[1]}\n")
+            else:
+                txt_file.write(f"{entry.msgid} -> {entry.msgstr}\n")
+
+def txt_to_po(txt_file):
+    with open(txt_file, "r") as file:
+        lines = file.readlines()
+
+    po = polib.POFile()
+    po.metadata = {"Content-Type": "text/plain; charset=utf-8"}
+
+    for i in range(len(lines)):
+        line = lines[i].strip()
+        if line:
+            msgid, msgstr = line.split(" -> ")
+            entry = polib.POEntry(msgid=msgid, msgstr=msgstr)
+
+            # Handle plural forms
+            if i + 1 < len(lines) and lines[i + 1].startswith(msgid + "_plural"):
+                i += 1
+                plural_line = lines[i].strip()
+                msgid_plural, msgstr_plural = plural_line.split(" -> ")
+                entry.msgid_plural = msgid_plural.replace("_
